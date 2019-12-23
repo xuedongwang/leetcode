d1fe646c40b3e6b0ef8ea552c8391bf5fe4aa8c0
w <email@wangxuedong.com>
2019-12-23 19:46:37 +0800

    auto commit

--- src/demo02/index.js
+++ src/demo02/index.js
@@ -4,20 +4,16 @@
 
 const reverse = function (x) {
   let ret = 0;
-  debugger;
   while (x !== 0) {
     const pop = x % 10;
     x = parseInt(x / 10);
-    // if (ret > Number.MAX_SAFE_INTEGER / 10 || (ret === Number.MAX_SAFE_INTEGER / 10 && (pop > Number.MAX_SAFE_INTEGER % 10))) { // to big
-    //   return 0;
-    // }
-    // if (ret < Number.MAX_SAFE_INTEGER / 10 || (ret === Number.MAX_SAFE_INTEGER / 10 && (pop < Number.MAX_SAFE_INTEGER % 10))) { // to small
-    //   return 0;
-    // }
-    ret = ret * 10 + pop;
-    if (ret > Number.MAX_SAFE_INTEGER || ret < Number.MIN_SAFE_INTEGER) {
+    if (ret > parseInt(Number.MAX_SAFE_INTEGER / 10) || (ret === parseInt(Number.MAX_SAFE_INTEGER / 10) && (pop > Number.MAX_SAFE_INTEGER % 10))) { // to big
       return 0;
     }
+    if (ret < parseInt(Number.MIN_SAFE_INTEGER / 10) || (ret === parseInt(Number.MIN_SAFE_INTEGER / 10) && (pop < Number.MIN_SAFE_INTEGER % 10))) { // to small
+      return 0;
+    }
+    ret = ret * 10 + pop;
   }
   return ret;
 }
--- src/demo02/index.spec.js
+++ src/demo02/index.spec.js
@@ -12,8 +12,14 @@ test('reverseNum(-12345678) revert is -87654321', () => {
   expect(index(-12345678)).toBe(-87654321);
 });
 
-test('reverseNum(9007199254740991) revert is 1990474529917009', () => {
-  expect(index(9007199254740991)).toBe(1990474529917009);
+test('reverseNum(1990474529917009) revert is 9007199254740991', () => {
+  expect(index(1990474529917009)).toBe(9007199254740991);
+});
+
+// 900719925474099
+
+test('reverseNum(-1990474529917009) revert is -9007199254740991', () => {
+  expect(index(-1990474529917009)).toBe(-9007199254740991);
 });
 
 test('reverseNum(1234567123456789) revert is 0', () => {
