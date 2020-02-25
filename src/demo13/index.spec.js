d719024a96b35861818be859b062e111f4227a76
Well <email@wangxuedong.com>
2020-02-25 16:00:31 +0800

    auto commit

--- src/demo13/index.spec.js
+++ src/demo13/index.spec.js
@@ -1,7 +1,23 @@
 
 const asteroidCollision = require('./index');
 
-test('asteroidCollision() return ', () => {
-  expect(asteroidCollision()).toBe();
+test('asteroidCollision([5, 10, -5]) return [5, 10]', () => {
+  expect(asteroidCollision([5, 10, -5])).toEqual([5, 10]);
+});
+
+test('asteroidCollision([8, -8]) return []', () => {
+  expect(asteroidCollision([8, -8])).toEqual([]);
+});
+
+test('asteroidCollision([10, 2, -5]) return [10]', () => {
+  expect(asteroidCollision([10, 2, -5])).toEqual([10]);
+});
+
+test('asteroidCollision([-2, -1, 1, 2]) return [-2, -1, 1, 2]', () => {
+  expect(asteroidCollision([-2, -1, 1, 2])).toEqual([-2, -1, 1, 2]);
+});
+
+test('asteroidCollision([]) return []', () => {
+  expect(asteroidCollision([])).toEqual([]);
 });
 
