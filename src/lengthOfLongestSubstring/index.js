11d21df66596b5fc1f4efbb89bdfc90c7c415a77
XueDong Wang <email@wangxuedong.com>
2020-06-30 20:56:09 +0800

    auto commit

--- src/lengthOfLongestSubstring/index.js
+++ src/lengthOfLongestSubstring/index.js
@@ -10,11 +10,10 @@ const lengthOfLongestSubstring = function (s) {
   let maxLength = 0;
   for (let i = 0; i < s.length; i++) {
     if (map.has(s[i])) {
-      left = map.get(s[i]) + 1;
-    } else {
-      map.set(s[i], i);
+      left = Math.max(map.get(s[i]), left + 1);
     }
-    maxLength = Math.max(maxLength, i - left);
+    map.set(s[i], i);
+    maxLength = Math.max(maxLength, i - left + 1);
   }
   return maxLength;
 }
--- src/lengthOfLongestSubstring/index.spec.js
+++ src/lengthOfLongestSubstring/index.spec.js
@@ -1,23 +1,23 @@
 
 const lengthOfLongestSubstring = require('./index');
 
-// test('lengthOfLongestSubstring(abcabcbb) return 3', () => {
-//   expect(lengthOfLongestSubstring('abcabcbb')).toBe(3);
-// });
+test('lengthOfLongestSubstring(abcabcbb) return 3', () => {
+  expect(lengthOfLongestSubstring('abcabcbb')).toBe(3);
+});
 
-// test('lengthOfLongestSubstring(bbbbb) return 1', () => {
-//   expect(lengthOfLongestSubstring('bbbbb')).toBe(1);
-// });
+test('lengthOfLongestSubstring(bbbbb) return 1', () => {
+  expect(lengthOfLongestSubstring('bbbbb')).toBe(1);
+});
 
-// test('lengthOfLongestSubstring(pwwkew) return 3', () => {
-//   expect(lengthOfLongestSubstring('pwwkew')).toBe(3);
-// });
+test('lengthOfLongestSubstring(pwwkew) return 3', () => {
+  expect(lengthOfLongestSubstring('pwwkew')).toBe(3);
+});
 
-// test('lengthOfLongestSubstring(aab) return 2', () => {
-//   expect(lengthOfLongestSubstring('aab')).toBe(2);
-// });
+test('lengthOfLongestSubstring(aab) return 2', () => {
+  expect(lengthOfLongestSubstring('aab')).toBe(2);
+});
 
-// test('lengthOfLongestSubstring(tmmzuxt) return 5', () => {
-//   expect(lengthOfLongestSubstring('tmmzuxt')).toBe(5);
-// });
+test('lengthOfLongestSubstring(tmmzuxt) return 5', () => {
+  expect(lengthOfLongestSubstring('tmmzuxt')).toBe(5);
+});
 
