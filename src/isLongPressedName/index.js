5d6f52a565cb3ba1f15d7a3457fc3782712874e7
Xuedong Wang <xuedongwang@tencent.com>
2020-10-21 18:25:23 +0800

    feat:quickSort

--- src/isLongPressedName/index.js
+++ src/isLongPressedName/index.js
@@ -5,21 +5,21 @@
 
 
 const isLongPressedName = function (name, typed) {
-  const n = name.length;
-  const m = typed.length;
+  const m = name.length;
+  const n = typed.length;
   let i = 0;
   let j = 0;
-  while (j < m) {
-    if (i < n && name[i] === typed[j]) {
+  while (j < n) {
+    if (i < m && name[i] === typed[j]) {
       i ++;
       j ++;
-    } else if (j > 0 && typed[j] === typed[j - 1]) {
-      j ++
+    } else if (j < n && typed[j] === typed[j - 1]) {
+      j ++;
     } else {
       return false;
     }
   }
-  return i === n;
+  return i === m;
 }
 
 module.exports = isLongPressedName;
---
+++ src/quickSort/index.js
@@ -0,0 +1,36 @@
+
+/**
+ * @ref: https://juejin.im/post/6844903938915827725
+ */
+
+const quickSort = function (arr, start = 0, end = arr.length - 1) {
+  if (end - start < 1) {
+    return arr;
+  }
+  const pivotIndex = partition(arr, start, end);
+  quickSort(arr,start, pivotIndex - 1);
+  quickSort(arr,pivotIndex + 1, end);
+  return arr;
+}
+
+
+function partition (arr, start, end) {
+  let j = start;
+  const pivot = arr[end];
+  for (let i = start; i <= end; i ++) {
+    if (arr[i] <= pivot) {
+      swap(arr, i, j);
+      j ++;
+    }
+  }
+  return j - 1;
+}
+
+
+function swap (arr, a, b) {
+  const temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+
+module.exports = quickSort;
