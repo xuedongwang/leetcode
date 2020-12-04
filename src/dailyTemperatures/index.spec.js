bb32911a3b44ccd86edc9d3776e11e53f82e7d46
Xuedong Wang <xuedongwang@tencent.com>
2020-12-04 17:03:38 +0800

    dailyTemperatures

---
+++ src/dailyTemperatures/index.spec.js
@@ -0,0 +1,7 @@
+
+const dailyTemperatures = require('./index');
+
+test(`dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]) return [1, 1, 4, 2, 1, 1, 0, 0]`, () => {
+  expect(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])).toEqual([1, 1, 4, 2, 1, 1, 0, 0]);
+});
+
