d842dc50478258be50f0a99d0037d3bff28f918c
Xuedong Wang <xuedongwang@tencent.com>
2021-01-22 14:39:40 +0800

    auto commit

---
+++ src/maximumWealth/index.spec.js
@@ -0,0 +1,14 @@
+
+const maximumWealth = require('./index');
+
+test(`maximumWealth([[1,2,3],[3,2,1]]) return 6`, () => {
+  expect(maximumWealth([[1,2,3],[3,2,1]])).toBe(6);
+});
+
+test(`maximumWealth([[1,5],[7,3],[3,5]]) return 10`, () => {
+  expect(maximumWealth([[1,5],[7,3],[3,5]])).toBe(10);
+});
+
+test(`maximumWealth([[2,8,7],[7,1,3],[1,9,5]]) return 17`, () => {
+  expect(maximumWealth([[2,8,7],[7,1,3],[1,9,5]])).toBe(17);
+});
---
+++ src/reverseLeftWords/index.js
@@ -0,0 +1,28 @@
+
+/**
+ * @ref: https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+ */
+
+const reverseLeftWords = function (s, n) {
+  let str1 = '';
+  let str2 = '';
+  for (let i = 0; i < s.length; i ++) {
+    if (i >= n) {
+      str1 += s[i];
+    } else {
+      str2 += s[i];
+    }
+  }
+  return str1 + str2;
+}
+
+module.exports = reverseLeftWords;
+
+// 示例 1：
+
+// 输入: s = "abcdefg", k = 2
+// 输出: "cdefgab"
+// 示例 2：
+
+// 输入: s = "lrloseumgh", k = 6
+// 输出: "umghlrlose"
