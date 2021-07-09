a0cc52f57db1a293d6ec816a76f997541f7d9bb1
Xuedong Wang <xuedongwang@tencent.com>
2021-07-09 17:11:33 +0800

    feat(moveZeroes): 增加 moveZeroes 并完成测试

---
+++ src/moveZeroes/index.js
@@ -0,0 +1,32 @@
+
+/**
+ * @ref: https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2ba4i/
+ */
+
+const moveZeroes = function (nums) {
+  let index = nums.length - 1;
+  while(index >= 0) {
+    if (nums[index] === 0) {
+      nums.push(nums[index]);
+      nums.splice(index, 1);
+    }
+    index --;
+  }
+  return nums;
+}
+
+module.exports = moveZeroes;
+
+/**
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+
+ */
