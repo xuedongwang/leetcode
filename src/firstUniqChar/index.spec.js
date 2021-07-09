65f9538afa9deee35f48357480528a013fcd5ff2
Xuedong Wang <xuedongwang@tencent.com>
2021-07-09 18:28:49 +0800

    feat(firstUniqChar): 增加 firstUniqChar 并完成测试

---
+++ src/firstUniqChar/index.spec.js
@@ -0,0 +1,11 @@
+
+const firstUniqChar = require('./index');
+
+test(`firstUniqChar('leetcode') return 0`, () => {
+  expect(firstUniqChar('leetcode')).toBe(0);
+});
+
+test(`firstUniqChar('loveleetcode') return 2`, () => {
+  expect(firstUniqChar('loveleetcode')).toBe(2);
+});
+
